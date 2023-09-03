c664edbca60d7326d40ec418f0a89970047086a5
lnotspotl <lnotspotl@gmail.com>
2023-09-03 11:16:06 +0200

    Add signed distance field computation

--- ocs2_robotic_examples/ocs2_legged_robot_ros/include/ocs2_legged_robot_ros/reference_generator/GridMapInterface.h
+++ ocs2_robotic_examples/ocs2_legged_robot_ros/include/ocs2_legged_robot_ros/reference_generator/GridMapInterface.h
@@ -10,6 +10,8 @@
 
 // gridmap
 #include <grid_map_ros/grid_map_ros.hpp>
+#include <grid_map_sdf/SignedDistanceField.hpp>
+
 #include <convex_plane_decomposition_msgs/PlanarTerrain.h>
 #include <convex_plane_decomposition_ros/MessageConversion.h>
 #include <convex_plane_decomposition/SegmentedPlaneProjection.h>
@@ -23,9 +25,10 @@ class GridMapInterface {
    public:
     GridMapInterface(::ros::NodeHandle &nh, std::string mapTopic, bool useGridMap);
     scalar_t atPositionElevation(scalar_t x, scalar_t y);
-    inline grid_map::GridMap &getMap() { return planarTerrainPtr->gridMap; }
+    inline grid_map::GridMap &getMap() { return planarTerrainPtr_->gridMap; }
+    inline grid_map::SignedDistanceField &getSDF() { return *sdfPtr_; }
     inline std::vector<convex_plane_decomposition::PlanarRegion> &getPlanarRegions() {
-        return planarTerrainPtr->planarRegions;
+        return planarTerrainPtr_->planarRegions;
     }
 
    private:
@@ -35,7 +38,8 @@ class GridMapInterface {
     ::ros::Subscriber mapSubscriber_;
     std::vector<std::string> layers_;
 
-    std::unique_ptr<convex_plane_decomposition::PlanarTerrain> planarTerrainPtr;
+    std::unique_ptr<convex_plane_decomposition::PlanarTerrain> planarTerrainPtr_;
+    std::unique_ptr<grid_map::SignedDistanceField> sdfPtr_;
 
     bool useGridmap_;
 };
--- ocs2_robotic_examples/ocs2_legged_robot_ros/src/reference_generator/GridMapInterface.cpp
+++ ocs2_robotic_examples/ocs2_legged_robot_ros/src/reference_generator/GridMapInterface.cpp
@@ -1,6 +1,7 @@
 #include "ocs2_legged_robot_ros/reference_generator/GridMapInterface.h"
 
 #include <iostream>
+#include <chrono>
 
 namespace ocs2 {
 namespace legged_robot {
@@ -30,19 +31,50 @@ GridMapInterface::GridMapInterface(::ros::NodeHandle &nh, std::string mapTopic, 
 
 void GridMapInterface::mapCallback(const convex_plane_decomposition_msgs::PlanarTerrain::ConstPtr &msg) {
 
-    if(!planarTerrainPtr) {
-        std::unique_ptr<convex_plane_decomposition::PlanarTerrain> newTerrain(
-            new convex_plane_decomposition::PlanarTerrain(convex_plane_decomposition::fromMessage(*msg)));
-        planarTerrainPtr.swap(newTerrain);
+    if (planarTerrainPtr_) {
+        return;
     }
+
+    auto convexStart = std::chrono::high_resolution_clock::now();
+    // Do convex plane decomposition
+    std::unique_ptr<convex_plane_decomposition::PlanarTerrain> newTerrain(
+        new convex_plane_decomposition::PlanarTerrain(convex_plane_decomposition::fromMessage(*msg)));
+    planarTerrainPtr_.swap(newTerrain);
+    auto convexEnd = std::chrono::high_resolution_clock::now();
+    std::cout << "Convex plane decomposition took "
+              << std::chrono::duration_cast<std::chrono::microseconds>(convexEnd - convexStart).count() << " us"
+              << std::endl;
+
+    auto sdfStart = std::chrono::high_resolution_clock::now();
+
+    auto &map = getMap();
+  
+    // Compute signed distance field
+    std::vector<std::string> layers{layers_[0]};
+    auto &elevationData = map.get(layers[0]);
+
+    // Inpaint if needed.
+    if (elevationData.hasNaN()) {
+        const float inpaint{elevationData.minCoeffOfFinites()};
+        elevationData = elevationData.unaryExpr([=](float v) { return std::isfinite(v)? v : inpaint; });
+    }
+    const float heightMargin{0.1};
+    const float minValue{elevationData.minCoeffOfFinites()};
+    const float maxValue{elevationData.maxCoeffOfFinites() + heightMargin};
+    std::unique_ptr<grid_map::SignedDistanceField> newSDF(new grid_map::SignedDistanceField(
+        map, layers_[0], minValue, maxValue));
+    sdfPtr_.swap(newSDF);
+
+    auto sdfEnd = std::chrono::high_resolution_clock::now();
+    std::cout << "Signed distance field computation took "
+              << std::chrono::duration_cast<std::chrono::microseconds>(sdfEnd - sdfStart).count() << " us" << std::endl;
+    std::cout << "Resolution: " << map.getResolution() << std::endl;
 }
 
 scalar_t GridMapInterface::atPositionElevation(scalar_t x, scalar_t y) {
     if(!useGridmap_) {
         return 0.0;
     }
-
-
 
     const auto &map = getMap();
     pos_[0] = x;
